7299674e41e5ffdd4544200721368e7d813cc733
sheng.lin <hangeng.lin@talebase.com>
2025-07-21 22:31:07 +0800

    1.解决冲突

---
+++ hf_service/hr_msg_api/src/main/java/com/example/model/dto/tEST.java
@@ -0,0 +1,15 @@
+package com.example.model.dto;
+
+/**
+ * @ClassName tEST
+ * @Author sheng.lin
+ * @Date 2025/7/21
+ * @Version 1.0
+ * @修改记录
+ **/
+/*
+ * projectName: healthyFood
+ * copyright(c) ©2003-2024 Talebase. All Rights Reserved.
+ */
+public class tEST {
+}
